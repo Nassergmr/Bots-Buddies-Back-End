6f44ee626bdefe07aa87729d981a82a01d460680
Nasser <elhammouminasser99@gmail.com>
2025-06-27 15:08:05 +0100

    commit number 6

--- server.js
+++ server.js
@@ -2,15 +2,38 @@ import { createServer } from "http";
 import { config } from "dotenv";
 import SocketServer from "./socket_server.js";
 
+// Log if there any errors on production
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received. Shutting down gracefully...");
+  server.close(() => {
+    console.log("Server terminated.");
+    process.exit(0);
+  });
+});
+
 // Load .env variables
 config();
 
+const PORT = process.env.PORT;
 const server = createServer();
 
-// Pass server to your socket logic
-SocketServer(server);
+console.log("ENV PORT:", PORT);
+console.log("Starting server...");
 
-const PORT = process.env.PORT || 3001;
+// Pass server to the SocketServer function
+try {
+  SocketServer(server);
+} catch (error) {
+  console.error("SocketServer crashed:", error);
+}
 
 server.listen(PORT, () => {
   console.log(`✅ Server is listening on port ${PORT}`);
