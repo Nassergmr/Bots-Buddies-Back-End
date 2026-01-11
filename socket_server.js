7ffdeff2d09cb20e290f6f2e995b44e8b6a9bda5
Nasser <elhammouminasser99@gmail.com>
2026-01-11 15:43:32 +0100

    commit

--- socket_server.js
+++ socket_server.js
@@ -12,7 +12,7 @@ export default async function SocketServer(server) {
   // Creating a new Socket.IO server instance and attaching it to the provided HTTP server
   const io = new Server(server, {
     cors: {
-      origin: "https://bots-buddies.vercel.app",
+      origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
   });
